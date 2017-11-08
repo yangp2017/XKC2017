eea2645c92a4e0f4fb4953b35a589f21f518afc0
新课程 <32903570+yangp2017@users.noreply.github.com>
2017-11-08 09:36:07 +0800

    del
    删除重新定义

--- xkh5/js/common_cadr/file.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-define(function(require,exports) {
-	exports.file= function() {
-		$(".ycfile").change(function(){
-			$(this).parent().parent().prev("input.file").first().val($(this).val());
-		})
-	}
-	exports.file();
-});
